e6c5c27736a16e67b363d51900f3728e07d4cf7a
Muhammad <accank72@gmail.com>
2018-08-03 08:49:09 +0800

    revisi script exercise-4.js

--- exercise-4.js
+++ exercise-4.js
@@ -1,17 +1,17 @@
-var tanggal = prompt("masukkan tanggal (1 - 31):", "");
-var bulan = prompt("Masukkan bulan (1-12):", "");
-var tahun = prompt("Masukkan tahun (1900 - 2200):", "");
+var tanggal = 2;// assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 3; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 1972; //assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
 switch(bulan){
-  case "1": { document.write(tanggal+ " Januari " +tahun); break; }
-  case "2": { document.write(tanggal + " Februari " + tahun); break; }
-  case "3": { document.write(tanggal + " Maret " + tahun); break; }
-  case "4": { document.write(tanggal + " April " + tahun); break; }
-  case "5": { document.write(tanggal + " Mei " + tahun); break; }
-  case "6": { document.write(tanggal + " Juni " + tahun); break; }
-  case "7": { document.write(tanggal + " Juli " + tahun); break; }
-  case "8": { document.write(tanggal + " Agustus " + tahun); break; }
-  case "9": { document.write(tanggal + " September " + tahun); break; }
-  case "10": { document.write(tanggal + " Oktober " + tahun); break; }
-  case "11": { document.write(tanggal + " Nopember " + tahun); break; }
-  case "12": { document.write(tanggal + " Desember " + tahun); break; }
+  case 1: { console.log(tanggal+ " Januari " +tahun); break; }
+  case 2: { console.log(tanggal + " Februari " + tahun); break; }
+  case 3: { console.log(tanggal + " Maret " + tahun); break; }
+  case 4: { console.log(tanggal + " April " + tahun); break; }
+  case 5: { console.log(tanggal + " Mei " + tahun); break; }
+  case 6: { console.log(tanggal + " Juni " + tahun); break; }
+  case 7: { console.log(tanggal + " Juli " + tahun); break; }
+  case 8: { console.log(tanggal + " Agustus " + tahun); break; }
+  case 9: { console.log(tanggal + " September " + tahun); break; }
+  case 10: { console.log(tanggal + " Oktober " + tahun); break; }
+  case 11: { console.log(tanggal + " Nopember " + tahun); break; }
+  case 12: { console.log(tanggal + " Desember " + tahun); break; }
 }
